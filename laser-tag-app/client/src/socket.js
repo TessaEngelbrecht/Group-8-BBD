1952830eea51f3ba0565d1d6875e56897bb627eb
TessaEngelbrecht <tessa.engelbrecht@gmail.com>
2025-06-30 12:28:02 +0200

    Connect frontend to deployed backend

--- laser-tag-app/client/src/socket.js
+++ laser-tag-app/client/src/socket.js
@@ -1,3 +1,6 @@
+// src/socket.js
 import { io } from 'socket.io-client';
-const socket = io('http://localhost:4000');
+
+const socket = io('https://group-8-bbd-production.up.railway.app'); // ✅ Use your Railway URL
+
 export default socket;
